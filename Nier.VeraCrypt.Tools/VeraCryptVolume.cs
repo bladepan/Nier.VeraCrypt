2e1623de0ee42d34a9c8d06c6d89a9293550877d
pan <panxiaozhong@gmail.com>
2021-07-03 21:23:27 -0700

    read data bytes

--- Nier.VeraCrypt.Tools/VeraCryptVolume.cs
+++ Nier.VeraCrypt.Tools/VeraCryptVolume.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace Nier.VeraCrypt.Tools
@@ -14,59 +16,105 @@ namespace Nier.VeraCrypt.Tools
     /// </summary>
     public class VeraCryptVolume
     {
-        private static readonly VolumeField s_magic = new VolumeField {Offset = 64, Size = 4};
-        private static readonly VolumeField s_volumeHeaderVersion = new VolumeField {Offset = 68, Size = 2};
-        private static readonly VolumeField s_minProgramVersion = new VolumeField {Offset = 70, Size = 2};
-        private static readonly VolumeField s_volumeSize = new VolumeField {Offset = 100, Size = 8};
-        private static readonly VolumeField s_masterKeyScopeOffset = new VolumeField {Offset = 108, Size = 8};
-        private static readonly VolumeField s_masterKeyEncryptionSize = new VolumeField {Offset = 116, Size = 8};
-        private static readonly VolumeField s_sectorSize = new VolumeField {Offset = 128, Size = 4};
-        private static readonly VolumeField s_keys = new VolumeField {Offset = 256, Size = 256};
+        private static readonly VolumeField s_salt = new() {Offset = 0, Size = 64};
+        private static readonly VolumeField s_magic = new() {Offset = 64, Size = 4};
+        private static readonly VolumeField s_volumeHeaderVersion = new() {Offset = 68, Size = 2};
+        private static readonly VolumeField s_minProgramVersion = new() {Offset = 70, Size = 2};
+        private static readonly VolumeField s_volumeSize = new() {Offset = 100, Size = 8};
+        private static readonly VolumeField s_masterKeyScopeOffset = new() {Offset = 108, Size = 8};
+        private static readonly VolumeField s_masterKeyEncryptionSize = new() {Offset = 116, Size = 8};
+        private static readonly VolumeField s_sectorSize = new() {Offset = 128, Size = 4};
+        private static readonly VolumeField s_keys = new() {Offset = 256, Size = 256};
         private static readonly IBitConverter s_bigEndianBitConverter = BitConverters.BigEndian();
+        private const int s_headerSize = 512;
 
-        public string ReadMagic(Span<byte> bytes)
+        public string Magic { get; }
+        public short VolumeHeaderVersion { get; }
+
+        public short MinProgramVersion { get; }
+
+        public long VolumeSize { get; }
+        public int SectorSize { get; }
+
+        public long MasterKeyScopeOffset { get; }
+
+        public long MasterKeyEncryptionSize { get; }
+
+        private XTS _dataCryptor;
+        private readonly FileStream _fileStream;
+
+        public VeraCryptVolume(string filePath, string password)
         {
-            return ReadAscii(bytes, s_magic);
+            _fileStream = File.OpenRead(filePath);
+            if (_fileStream.Length <= s_headerSize)
+            {
+                throw new Exception("Invalid volume, size is less than header.");
+            }
+            var header = new Span<byte>(new byte[s_headerSize]);
+            ReadAll(_fileStream, header);
+
+            var salt = ReadBytes(header, s_salt);
+
+            var rfc2898DeriveBytes =
+                new Rfc2898DeriveBytes(password, salt.ToArray(), 500000, HashAlgorithmName.SHA512);
+            byte[] keyBytes = rfc2898DeriveBytes.GetBytes(64);
+
+            int keySize = keyBytes.Length / 2;
+            var key1 = new AesCipher(keyBytes[..keySize]);
+            var key2 = new AesCipher(keyBytes[keySize..]);
+            var xts = new XTS(key1, key2);
+            var decryptedHeaderBytes = new Span<byte>(new byte[s_headerSize]);
+            xts.Decrypt(header.Slice(64), decryptedHeaderBytes.Slice(64), 0);
+            Magic = ReadAscii(decryptedHeaderBytes, s_magic);
+            if (Magic != "VERA")
+            {
+                throw new Exception("Invalid magic field " + Magic);
+            }
+
+            VolumeHeaderVersion = ReadShort(decryptedHeaderBytes, s_volumeHeaderVersion);
+            MinProgramVersion = ReadShort(decryptedHeaderBytes, s_minProgramVersion);
+            VolumeSize = ReadLong(decryptedHeaderBytes, s_volumeSize);
+            SectorSize = ReadInt(decryptedHeaderBytes, s_sectorSize);
+            MasterKeyScopeOffset = ReadLong(decryptedHeaderBytes, s_masterKeyScopeOffset);
+            MasterKeyEncryptionSize = ReadLong(decryptedHeaderBytes, s_masterKeyEncryptionSize);
+
+            var dataKeys = ReadBytes(decryptedHeaderBytes, s_keys);
+            var dataKey1 = new AesCipher(dataKeys.Slice(0, keySize).ToArray());
+            var dataKey2 = new AesCipher(dataKeys.Slice(keySize, keySize).ToArray());
+            _dataCryptor = new XTS(dataKey1, dataKey2);
         }
 
-        public short VolumeHeaderVersion(Span<byte> bytes)
+        public void ReadAllDataBytes(Span<byte> buffer)
         {
-            return ReadShort(bytes, s_volumeHeaderVersion);
+            _fileStream.Seek(MasterKeyScopeOffset, SeekOrigin.Begin);
+            // read sector by sector
+            ulong sectorNum = 0;
+            int offset = 0;
+            Span<byte> sectorBuffer = new Span<byte>(new byte[SectorSize]);
+            while (offset < buffer.Length)
+            {
+                ReadAll(_fileStream, sectorBuffer);
+                _dataCryptor.Decrypt(sectorBuffer, buffer.Slice(offset, SectorSize), sectorNum);
+                sectorNum++;
+                offset += SectorSize;
+            }
         }
 
-        public short MinProgramVersion(Span<byte> bytes)
+        private void ReadAll(FileStream fileStream, Span<byte> buffer)
         {
-            return ReadShort(bytes, s_minProgramVersion);
-        }
-
-        public long ReadVolumeSize(Span<byte> bytes)
-        {
-            return ReadLong(bytes, s_volumeSize);
-        }
-
-        public int ReadSectorSize(Span<byte> bytes)
-        {
-            return ReadInt(bytes, s_sectorSize);
-        }
-
-        public long ReadMasterKeyScopeOffset(Span<byte> bytes)
-        {
-            return ReadLong(bytes, s_masterKeyScopeOffset);
-        }
-
-        public long ReadMasterKeyEncryptionSize(Span<byte> bytes)
-        {
-            return ReadLong(bytes, s_masterKeyEncryptionSize);
-        }
-
-        public Span<byte> ReadKeys(Span<byte> bytes)
-        {
-            return ReadBytes(bytes, s_keys);
+            int dataRead = 0;
+            var slice = buffer;
+            while (dataRead < buffer.Length)
+            {
+                int currentRead = fileStream.Read(slice);
+                dataRead += currentRead;
+                slice = slice.Slice(currentRead);
+            }
         }
 
         private Span<byte> ReadBytes(Span<byte> bytes, VolumeField volumeField)
         {
-            return bytes.Slice(volumeField.Offset - 64, volumeField.Size);
+            return bytes.Slice(volumeField.Offset, volumeField.Size);
         }
 
         private long ReadLong(Span<byte> bytes, VolumeField volumeField)
